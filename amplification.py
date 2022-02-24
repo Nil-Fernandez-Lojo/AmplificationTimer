9d681667cd02643122dbf012a0c33c6f76de4a81
nilfernandezlojo <nilfernandezlojo@gmail.com>
2022-02-24 11:02:40 +0000

    put everything up to date

--- amplification.py
+++ amplification.py
@@ -1,3 +1,5 @@
+import copy
+
 from decorator_equality import class_equality_attributes
 
 @class_equality_attributes
@@ -24,24 +26,59 @@ class Amplification():
 	get_genes: returns the genes that are encoded in teh segments
 	to_dict: returns a dictionary encoding the amplification
 	"""
-	def __init__(self,chromosome,arm,segments):
+	def __init__(self,chromosome,arm,segments,threshold_amplification,clinical_data,config):
 		self.chromosome = chromosome
 		self.arm = arm
 		self.segments = segments
-		self.infered_time = None
-	
+		self.config = config
+		self.threshold_amplification = threshold_amplification
+		self.clinical_data = clinical_data
+		self.config = config
+		self.oncogenes = []
+
 	def add_segment(self,segment):
 		self.segments.append(segment)
 
 	def get_genes(self):
 		genes = []
 		for segment in self.segments:
+			for gene in segment.genes:
+				if gene not in genes:
+					genes.append(gene)
 			genes += segment.genes
 		return genes
+
+	def set_oncogenes(self):
+		self.oncogenes = []
+		genes = self.get_genes()
+		for gene in genes:
+			if isinstance(gene.entrez_id,list):
+				list_entrez_id = gene.entrez_id
+			else:
+				list_entrez_id = [gene.entrez_id]
+			for entrez_id in list_entrez_id:
+				if entrez_id in self.config['oncogenes']["Entrez GeneId"].values and (gene not in self.oncogenes):
+					self.oncogenes.append(gene)				
+		return copy.deepcopy(self.oncogenes)
+
+	def get_mean_ploidy(self):
+		tot_len = 0
+		number_bases = 0
+		for segment in self.segments:
+			tot_cn = segment.major_cn + segment.minor_cn
+			len_segment = segment.get_length()
+			number_bases += tot_cn*len_segment
+			tot_len += len_segment
+		return number_bases/tot_len
 
 	def to_dict(self):
 		dic = dict()
 		dic['chromosome'] = str(self.chromosome)
 		dic['arm'] = self.arm
 		dic['segments'] = [segment.to_dict() for segment in self.segments]
+		dic['oncogenes'] = [gene.to_dict() for gene in self.oncogenes]
+		dic['threshold_amplification'] = self.threshold_amplification
+		dic['clinical_data'] = self.clinical_data
+		# TODO: not good practice, should change this:
+		# self.config is not added since it contains objects of type PATH that can't be saved in json file later
 		return dic
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,14 +1,13 @@
 # author: Nil Fernandez Lojo
+from decorator_equality import class_equality_attributes #TODO remove
 import pandas as pd
 from pathlib import Path
-from Sample import Sample
+from sample import Sample
 from chromosome import Chromosome
+#from inference import inference_t
 from pyensembl import EnsemblRelease
-from models import Model1, Model2,Model3
-
 import numpy as np 
-import arviz as az
-import matplotlib.pyplot as plt
+import json
 
 
 #Paths input data
@@ -16,52 +15,66 @@ path_data = Path("../memoire/data")
 path_chromosome_arm_length = "hg19_chr_arm_length.tsv"
 path_summary_table = path_data / "summary_table_combined_annotations_v4.txt" 
 path_ensembl_to_entrez= Path("ensembl_to_entrez.tsv")
+path_oncognes = path_data / "Census_allTue Nov 24 18_02_37 2020.csv"
 folder_cna = path_data / "consensus.20170119.somatic.cna.annotated"
-folder_snv_green = path_data / "final_consensus_snv_indel_passonly_icgc.public" / "snv_mnv"
+folder_snv_white = path_data / "final_consensus_snv_indel_passonly_icgc.public" / "snv_mnv"
 folder_snv_gray = path_data / "final_consensus_snv_indel_passonly_icgc.public" / "graylist"/ "snv_mnv"
+folder_subclonal_structure = Path("data/subclonal.structures")
+
 
 suffix_cnas = ".consensus.20170119.somatic.cna.annotated.txt"
 suffix_snvs = ".consensus.20160830.somatic.snv_mnv.vcf"
+suffix_subclonal_structure = "_subclonal_structure.txt"
 
+vcf_column_names = ["CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO"]
+vcf_prefix_counts = {"ref_count": "t_ref_count=", "alt_count" : "t_alt_count="}
 #Path out
 folder_preprocessed_data = Path('preprocessed_data')
+amplifications_file = folder_preprocessed_data/"amplifications.json"
 
+cores = 10
 
 config = dict()
+config['vcf_column_names'] = vcf_column_names
+config['vcf_prefix_counts'] = vcf_prefix_counts
 config['folder_cna'] = folder_cna
-config['folder_snv_green'] = folder_snv_green
+config['folder_snv_white'] = folder_snv_white
 config['folder_snv_gray'] = folder_snv_gray
 config['suffix_cnas'] = suffix_cnas
 config['suffix_snvs'] = suffix_snvs
-config['suffix_snvs'] = suffix_snvs
+config['suffix_subclonal_structure'] = suffix_subclonal_structure
 config['folder_preprocessed_data'] = folder_preprocessed_data
+config['folder_subclonal_structure'] = folder_subclonal_structure
 config['summary_table'] = pd.read_csv(path_summary_table, sep='\t')
 config['ensembl_to_entrez'] = pd.read_csv(path_ensembl_to_entrez, sep='\t') #TODO what to do with duplicates
 config['ensembl'] = EnsemblRelease(75) # TODO: I need to justify why version 75
 config['chromosome_arm_length'] = pd.read_csv(path_chromosome_arm_length, sep='\t')
 config['chromosome_arm_length'].rename(columns={'chrom': 'chromosome'},inplace=True)
-
+config['oncogenes'] = pd.read_csv(path_oncognes)
 N_samples = config['summary_table'].shape[0]
-for i in range(13,N_samples):	
+
+#TODO: should change place
+config['cum_length_chr'] = np.zeros(25)
+cum_sum = 0
+for i,chromosome in enumerate(list(range(1,23)) + ['X','Y']):
+	lengths = config['chromosome_arm_length']['length'][ config['chromosome_arm_length']['chromosome'] == str(chromosome)]
+	config['cum_length_chr'][i] = cum_sum
+	cum_sum += lengths.sum()
+config['cum_length_chr'][-1] = cum_sum
+
+
+n_MCMC_iterations = 10000
+folder_models = Path("inferred_times")
+amplifications = []
+for i in range(N_samples):
 	print(i, config['summary_table']['samplename'][i])
 	s = Sample(config,config['summary_table']['samplename'][i],save=False)
-	x = Model1(s.amplifications[0],s.mutation_rate,s.clinical_data)
-	t = np.linspace(0,1,100)
-	plt.plot(t,x.get_analytical_posterior(t))
-	print(x.get_approximate_MAP())
-	trace = x.get_MCMC_samples_posterior(20000)
-	az.plot_trace(trace)
-	print(az.summary(trace, round_to=2))
-	print('Model 2')
-	x2 = Model2(s.amplifications[0],s.mutation_rate,s.clinical_data)
-	print(x2.get_approximate_MAP())
-	trace2 = x2.get_MCMC_samples_posterior(20000)
-	az.plot_trace(trace2)
-	print(az.summary(trace2, round_to=2))
-	x3 = Model3(s.amplifications[0],s.mutation_rate,s.clinical_data)
-	print(x3.get_approximate_MAP())
-	trace3 = x3.get_MCMC_samples_posterior(20000)
-	az.plot_trace(trace3)
-	print(az.summary(trace3, round_to=2))
-	plt.show()
-	exit()
+	amplifications.extend(a.to_dict() for a in s.amplifications)
+	with open(amplifications_file, 'w') as buff:
+		json.dump(amplifications, buff, indent=4)
+	continue
+	if len(s.amplifications)>0 and (s.snv_type in ["white", "gray"]):
+	 	for j in range(len(s.amplifications)):
+	 		for subclonality_modelled in [False, True]:
+	 			for model_index in range(1,5):
+	 				inference_t(s,j,model_index,subclonality_modelled,n_MCMC_iterations,folder_models,cores)
