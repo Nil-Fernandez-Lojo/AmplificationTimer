1c2bb7471ff4bb43682837d8304f8fe5f85dbd2a
nilfernandezlojo <nilfernandezlojo@gmail.com>
2022-03-25 11:22:32 +0100

    corrected error import in __init__

--- AmplificationTimer/AmplificationTimerObjects/__init__.py
+++ AmplificationTimer/AmplificationTimerObjects/__init__.py
@@ -7,4 +7,4 @@ from .segment import Segment
 from .mutation_rate import MutationRate
 from .snv import SNV
 from .plot import plot_cn, plot_normalised_mu,plot_rainfall
-from compute_normalised_mu import compute_normalised_mu, compute_normalised_mu_one_window
+from .compute_normalised_mu import compute_normalised_mu, compute_normalised_mu_one_window
